d6f45ead9fd2ee1a60a62a0461124a8b49645d81
Abdur Razzak <razzak606@gmail.com>
2025-06-27 19:32:21 +0600

    --

--- static/src/js/dynamic_snippets.js
+++ static/src/js/dynamic_snippets.js
@@ -6,7 +6,7 @@ import { rpc } from "@web/core/network/rpc";
 publicWidget.registry.Products = publicWidget.Widget.extend({
     selector: '.dynamic-products',
     start() {
-    
+
         let products = this.el.querySelector('#dynamic-products')
         let data = rpc('/all_products', {
             'res_id': '',
