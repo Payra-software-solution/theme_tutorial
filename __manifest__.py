905a18e768c6c0bd559d742c533dce2974430d05
tanvir321110 <tanvirahmed321110@gmail.com>
2025-07-07 18:07:26 +0600

    first commit

--- __manifest__.py
+++ __manifest__.py
@@ -10,11 +10,26 @@
         'views/footer.xml',
         'views/snippets/property_agents.xml',
         'views/snippets/dynamic_products.xml',
+        'views/snippets/blogs.xml',
+        'views/snippets/testimonial.xml',
         'views/snippets/snippets.xml',
+    ],
+    'images': [
+        'static/description/cobalt_screenshot.jpg',
     ],
     'assets': {
         'web.assets_frontend': [
+            # ==============   For Main Css   ===============
             'theme_tutorial/static/src/css/styles.css',
+            'theme_tutorial/static/src/css/common.css',
+
+            # ==============   For All Snippets Css File  ===============
+            'theme_tutorial/static/src/css/snippet_css/testimonial.css',
+
+            # ==============   For Snippets Css Files   ===============
+
+
+            # ==============   For Js   ===============
             'theme_tutorial/static/src/js/dynamic_snippets.js',
         ]
     },
